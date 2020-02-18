c784930826f95cec2e211b5a0f47b12eafe4088b
Vincent Wren <wrenvin@gmail.com>
2020-02-18 08:31:07 -0600

    Updated for Classes

--- Art.py
+++ Art.py
@@ -7,30 +7,38 @@ gameDisplay = pygame.display.set_mode((width,height))#Makes window
 pygame.display.set_caption('Demo')#Titles window
 clock = pygame.time.Clock()#Keeps time for pygame
 
-end = False
+
 gameDisplay.fill((0,0,255))
 
-def drawShape():
-    pygame.draw.rect(gameDisplay, (randint(0,255),randint(0,255),randint(0,255)),
-     (randint(0,0.75*width), randint(0,0.75*height), randint(0,0.5*width), randint(0,0.5*width)))
-    pygame.draw.circle(gameDisplay, (randint(0,255),randint(0,255),randint(0,255)),
-     ((randint(0,0.9*width)),(randint(0,0.9*height))), (randint(0,0.2*width)), 0)
-  
+class Draw:
+    def __init__(self, x, y, lastx, lasty):
+        self.x = x
+        self.y = y
+        self.lastx = lastx
+        self.lasty = lasty
 
+
+
+end = False
+down = False
 while not end:
+    x, y = pygame.mouse.get_pos()
     #drawShape()
     #pygame.draw.rect(gameDisplay, (0,255,0), (10, 10, 4, 4))
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            drawShape()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            gameDisplay.fill((0,0,255))
+            down = True
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            down = False
+
+        if down:
+            pass
+
         if event.type == pygame.QUIT:
             end = True
-
-        print(event)
-
-    pygame.display.update()
+    lastx, lasty = pygame.mouse.get_pos()
+    pygame.display.flip()
 
     clock.tick(60)
 
